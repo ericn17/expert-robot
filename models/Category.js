914a89ad6633c51edff267acdf7afbfa48ec3d91
Eric Nguyen <15enguyen4@gmail.com>
2022-06-26 14:09:32 -0700

    added models

--- models/Category.js
+++ models/Category.js
@@ -2,19 +2,25 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
-class Tag extends Model {}
+class Category extends Model {}
 
-Tag.init(
+Category.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'tag',
+    modelName: 'category',
   }
 );
 
-module.exports = Tag;
+module.exports = Category;
